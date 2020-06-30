fc2cb0119ae581dffb3e7ce68dd2e906c9a4d23b
Nayanapm1 <NayanaPampauraMadali@my.unt.edu>
2020-06-30 15:44:18 -0500

    Removed unwanted pages

--- rollcallmodule/models.py
+++ rollcallmodule/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
+
+# Create your models here.
+class History(models.Model):
+    Attendance = models.CharField(max_length=45)
+    AttendanceID = models.AutoField(primary_key=True)
+    CourseName = models.CharField(max_length=45)
+    Date = models.DateField(max_length=45)
+    Password = models.CharField(max_length=45)
+    StudentName = models.CharField(max_length=45)
+    StudentNo = models.CharField(max_length=45)
+    TermName = models.CharField(max_length=45)
+    Username = models.CharField(max_length=45)
+
+    class Meta:
+        db_table = 'History'
+
+from django.db import models
+
 # Create your models here.
