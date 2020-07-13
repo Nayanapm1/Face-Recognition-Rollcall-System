875ed11a7e70db76726886fc0c066e105a1bf468
Nayanapm1 <NayanaPampauraMadali@my.unt.edu>
2020-07-13 12:15:30 -0500

    Assignment 4 Updated

--- selectcourse/models.py
+++ selectcourse/models.py
@@ -2,24 +2,24 @@ from django.db import models
 
 # Create your models here.
 
-class Tterm(models.Model):
+class term(models.Model):
     termid = models.AutoField(primary_key=True)
     termname = models.CharField(max_length=100)
 
     class Meta:
         db_table='term'
 
-class Tcourse(models.Model):
+class course(models.Model):
     courseid = models.AutoField(primary_key=True)
     coursename = models.CharField(max_length=200)
 
     class Meta:
         db_table='course'
 
-class TSelectCourse(models.Model):
+class selectcourse(models.Model):
     scid = models.AutoField(primary_key=True)
-    term = models.ForeignKey('Tterm', on_delete=models.CASCADE, db_column='termid')
-    course = models.ForeignKey('Tcourse', on_delete=models.CASCADE, db_column='courseid')
+    term = models.ForeignKey('term', on_delete=models.CASCADE, db_column='termid')
+    course = models.ForeignKey('course', on_delete=models.CASCADE, db_column='courseid')
     startdate = models.DateTimeField(null=True)
     enddate = models.DateTimeField(null=True)
 
