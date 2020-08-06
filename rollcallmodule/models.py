dcc423dd07cdd2970e5bc94af21a4b20303fc98a
Nayanapm1 <NayanaPampauraMadali@my.unt.edu>
2020-08-06 16:43:52 -0500

    Final Changes

--- rollcallmodule/models.py
+++ rollcallmodule/models.py
@@ -1,6 +1,5 @@
 from django.db import models
 from selectcourse import models as selectcourse_models
-import datetime as d
 
 
 # Create your models here.
@@ -14,29 +13,6 @@ class studentrec(models.Model):
     class Meta:
         db_table = 'studentrec'
 
-    # def getrate(self):
-    #     attdate = attendate.objects.filter(selectcourse=self.selectcourse)
-    #     attlist = []
-    #     rate = "0%"
-    #     attendlist = []
-    #     for row in attdate:
-    #         attdateid = row.attdateid
-    #         attrec = attendancerec.objects.filter(attendetails=row, studetails=self)
-    #         attendrec = attendancerec.objects.filter(attendetails=row, studetails=self, attendance=1)
-    #
-    #         if attrec.count() != 0:
-    #             attlist.append(attrec[0])
-    #         if attendrec.count() != 0:
-    #             attendlist.append(attendrec[0])
-
-        # if attdate.count() != 0:
-        #     totalcount = attdate.count()
-        #     acount = len(attendlist)
-        #     if totalcount != 0:
-        #         acrate = acount / totalcount
-        #         acrate = '%.2f%%' % (acrate * 100)
-        # return acrate
-
 class attendate(models.Model):
     attdateid = models.AutoField(primary_key=True)
     attdate = models.DateTimeField()
@@ -45,23 +21,11 @@ class attendate(models.Model):
     class Meta:
         db_table='attendate'
 
-    # def StartNewDate(scid):
-    #     nowtime = d.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
-    #     attdate = attendate()
-    #     attdate.attdate = nowtime
-    #
-    #     sc = selectcourse_models.objects.get(scid=scid)
-    #     attdate.selectcourse = sc
-    #     attdate.save()
-    #     attdateid = attdate.attdateid
-    #     return attdateid
-
     def getmaxid(scid1):
         sql="select * from attendate where scid="+str(scid1)+" order by attdateid desc limit 1"
         lastdate=attendate.objects.raw(sql)
         attdateid=lastdate[0].attdateid
         return attdateid
-
 
 class attendancerec(models.Model):
     attendanceID = models.AutoField(primary_key=True)
@@ -79,30 +43,18 @@ class attendancerec(models.Model):
 
     def atthist(scid1, attdateid):
         rollcalldatelist = attendate.objects.filter(selectcourse=scid1)
- #       attdatelist = attendate.objects.filter(selectcourse=scid)
         if attdateid == 0:
             tattdateid = attendate.getmaxid(scid1)
-   #         attdate = attendate.objects.get(attdateid=attdateid)
             tattlist = attendancerec.objects.filter(attendate=tattdateid)
         else:
             tattlist = attendancerec.objects.filter(attdateid=attdateid)
-   #         attlist = attendancerec.objects.filter(attendetails=attdate)
         context = {'rollcalldatelist': rollcalldatelist, 'attreclist': tattlist, 'tattdateid': tattdateid}
         return context
 
     def statics(attdateid):
-        #attdate = attendate.objects.get(attdateid=attdateid)
         totalstu = attendancerec.objects.filter(attdateid=attdateid)
         attend = attendancerec.objects.filter(attendetails=totalstu, tiscall=1)
         rate = attend.count()/totalstu.count()
         rate = '%.2f%%' % (rate * 100)
         statistic = {'totalstu': totalstu.count(), 'attend': attend.count(), 'rate': rate}
         return statistic
-        # if total.count() == 0:
-        #     rate = "0%"
-        # else:
-        #     rate = attended.count() / total.count()
-        #     rate = '%.2f%%' % (rate * 100)
-        #
-        # stats = {'total': total.count(), 'attended': attended.count(), 'rate': rate}
-        # return stats
